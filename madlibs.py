0911a2e3b22bb80f0760aac983ab8754777f8795
Owner <owner@kevin-mbp.myfiosgateway.com>
2021-03-12 16:30:33 -0500

    Completed madlibs part of lab

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Give me a noun... ")
+adjective = input("Give me an adjective... ")
+adverb = input("Give me an adverb... ")
+verb = input ("Give me a verb... ")
+
+print ("Riding along with " + str(noun) + ",")
+print ("Above a " + str(adjective) + "bridge.")
+print ("Walking" + str(adverb) + "with the guys,")
+print ( str(verb) + " at a beach ")
